3a25bedd2afd0cfe737383d0e23e29ef00da389a
Govind-ravi <govindnr20122001@gmail.com>
2024-12-24 12:20:37 +0530

    npm re installed

--- E-Commerce/src/firebase.js
+++ E-Commerce/src/firebase.js
@@ -1,15 +1,15 @@
-import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA62AU3Uyo-KGi8YBu9Y1-Qjm-GHljAXKg",
-    authDomain: "ecommerce-d9897.firebaseapp.com",
-    projectId: "ecommerce-d9897",
-    storageBucket: "ecommerce-d9897.appspot.com",
-    messagingSenderId: "393675276960",
-    appId: "1:393675276960:web:ac9168d654d6e95ff9ffc5",
-    measurementId: "G-W5E0FR7G27"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "ecommerce-d9897.firebaseapp.com",
+  projectId: "ecommerce-d9897",
+  storageBucket: "ecommerce-d9897.appspot.com",
+  messagingSenderId: "393675276960",
+  appId: "1:393675276960:web:ac9168d654d6e95ff9ffc5",
+  measurementId: "G-W5E0FR7G27",
+};
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
